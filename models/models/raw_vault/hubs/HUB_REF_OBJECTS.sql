b7a1a9363ba1f4a45c38135a0991f02a8912b1e3
SimonSterckxx <114815407+SimonSterckxx@users.noreply.github.com>
2026-01-15 15:04:09 +0100

    [VaultSpeed Lite] Generated dbt models from VaultSpeed

---
+++ models/models/raw_vault/hubs/HUB_REF_OBJECTS.sql
@@ -0,0 +1,11 @@
+{%- set source_model = "stg_refobjects" -%}
+{%- set src_pk = "REFOBJECTS_HK" -%}
+{%- set src_nk = "REFOBJECTS_BK" -%}
+{%- set src_ldts = "load_date" -%}
+{%- set src_source = "record_source" -%}
+
+{{ automate_dv.hub(source_model=source_model,
+                   src_pk=src_pk,
+                   src_nk=src_nk,
+                   src_ldts=src_ldts,
+                   src_source=src_source) }}
