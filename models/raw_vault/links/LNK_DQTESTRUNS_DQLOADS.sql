19584400d7a60bdb21a1c0534c744f938519a9f4
SimonSterckxx <simon.sterckx@student.ucll.be>
2026-01-16 10:36:33 +0100

    [VaultSpeed Lite] Generated dbt models from VaultSpeed

---
+++ models/raw_vault/links/LNK_DQTESTRUNS_DQLOADS.sql
@@ -0,0 +1,11 @@
+{%- set source_model = "stg_lnk_dqtestruns_dqloads" -%}
+{%- set src_pk = "LNK_DQTESTRUNS_DQLOADS_HK" -%}
+{%- set src_fk = ["LNK_DQTESTRUNS_DQLOADS_FK1_HK", "LNK_DQTESTRUNS_DQLOADS_FK2_HK"] -%}
+{%- set src_ldts = "load_date" -%}
+{%- set src_source = "record_source" -%}
+
+{{ automate_dv.link(source_model=source_model,
+                    src_pk=src_pk,
+                    src_fk=src_fk,
+                    src_ldts=src_ldts,
+                    src_source=src_source) }}
